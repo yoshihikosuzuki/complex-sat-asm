f0198439a62b255068478f72ba7ce7fcac6ea471
yoshihikosuzuki <ys.neoteny@gmail.com>
2020-05-30 16:21:06 +0900

    refactor unit visualizer

--- src/visualizer/__init__.py
+++ src/visualizer/__init__.py
@@ -1,2 +1,1 @@
-from .read import TRReadViewer
-from .unit import scatter_ulen_comp, plot_ulen_transition, plot_ulens_count, plot_ulens_tot
+from . import read, unit
--- src/visualizer/unit.py
+++ src/visualizer/unit.py
@@ -1,73 +1,89 @@
-from typing import Optional, List, Tuple, Mapping
+from typing import Union, Optional, List
 from collections import Counter
 from BITS.plot.plotly import make_hist, make_scatter, make_layout, show_plot
 from ..type import TRRead
 
 
-def read_to_ulen_comp(read: TRRead) -> Mapping[int, int]:
-    return Counter([unit.length for unit in read.units]).most_common()
-
-
-def scatter_ulen_comp(read: TRRead):
-    """Draw a unit length vs unit length * count plot for a single read."""
-    comp = sorted(read_to_ulen_comp(read))
-    show_plot([make_scatter([ulen for ulen, count in comp],
-                            [ulen * count for ulen, count in comp],
-                            mode="lines+markers", marker_size=8, show_legend=False)],
-              make_layout(x_title="Unit length [bp]",
-                          y_title="Total length of the units of the length [bp]"))
-
-
-# TODO: consider removing this
-def scatter_ulen_comps(reads: List[TRRead]):
-    """Draw a unit length vs unit length * count plot for multiple reads."""
-    comps = [sorted(read_to_ulen_comp(read)) for read in reads]
-    show_plot([make_scatter([ulen for ulen, count in comp],
-                            [ulen * count for ulen, count in comp],
-                            mode="lines+markers", marker_size=6, name=f"read {reads[i].id}")
-               for i, comp in enumerate(comps)],
-              make_layout(x_title="Unit length [bp]",
-                          y_title="Total length of the units of the length [bp]"))
-
-
-def plot_ulen_transition(read: TRRead, out_fname: Optional[str] = None):
-    show_plot([make_scatter([unit.start for unit in read.units],
-                            [unit.length for unit in read.units])],
+def plot_ulen_transition(read: TRRead):
+    """Show positional distribution of unit lengths in `read."""
+    assert len(read.units) > 0, "No units to show"
+    show_plot(make_scatter(**dict(zip(('x', 'y'),
+                                      zip(*[x for unit in read.units
+                                            for x in [(unit.start, unit.length),
+                                                      (unit.end, unit.length),
+                                                      (None, None)]]))),
+                           mode="lines+markers"),
               make_layout(title=f"Read {read.id} (strand={read.strand})",
-                          x_title="Start position on the read",
+                          x_title="Start position",
                           y_title="Unit length [bp]",
-                          x_range=(0, read.length)),
-              out_fname=out_fname)
+                          x_range=(0, read.length)))
 
 
-def reads_to_ulens_count(reads: List[TRRead]) -> Mapping[int, int]:
-    return Counter([unit.length for read in reads for unit in read.units])
+def plot_ulen_composition(reads: Union[TRRead, List[TRRead]],
+                          by: str = "total"):
+    """Show composition of unit lengths in each of `reads`.
 
+    positional arguments:
+      @ reads : TRRead object of a list of TRRead objects.
 
-def reads_to_ulens_tot(reads: List[TRRead]) -> List[Tuple[int, int]]:
-    return sorted([(ulen, ulen * count)
-                   for ulen, count in reads_to_ulens_count(reads).items()])
+    optional arguments:
+      @ by             : Must be one of {"total", "count"}.
+    """
+    assert by in ("total", "count"), "`by` must be 'total' or 'count'"
+    if isinstance(reads, TRRead):
+        reads = [reads]
+    for read in reads:
+        assert len(read.units) > 0, f"Read {read.id}: no units to show"
 
-
-# TODO: merge two functions below
-def plot_ulens_count(reads: List[TRRead], min_ulen: int = 50):
-    show_plot(make_hist(list(filter(lambda ulen: ulen >= min_ulen,
-                                    [unit.length for read in reads for unit in read.units])),
-                        bin_size=1),
-              make_layout(title=f"Unit count for each unit length (>{min_ulen} bp unit)",
+    comps = {read.id: sorted(Counter([unit.length for unit in read.units]).items())
+             for read in reads}
+    if by == "total":
+        comps = {read_id: [(ulen, ulen * count) for ulen, count in counts]
+                 for read_id, counts in comps.items()}
+    show_plot([make_scatter(**dict(zip(('x', 'y'), zip(*comps[read.id]))),
+                            mode="lines+markers",
+                            marker_size=6,
+                            name=f"Read {read.id}",
+                            show_legend=True)
+               for read in reads],
+              make_layout(title="Composition of unit lengths",
                           x_title="Unit length [bp]",
-                          y_title="Unit count",
-                          x_range=(1, None)))
+                          y_title=("Total bases [bp]" if by == "total"
+                                   else "Frequency")))
 
 
-def plot_ulens_tot(reads: List[TRRead],
-                   min_ulen: Optional[int] = 1,
-                   max_ulen: Optional[int] = None,
-                   out_fname: Optional[str] = None):
-    show_plot(make_scatter(**dict(zip(('x', 'y'), zip(*reads_to_ulens_tot(reads)))),
-                           mode="lines"),
-              make_layout(title="Total length for each unit length",
+def plot_ulen_dist(reads: List[TRRead],
+                   by: str = "total",
+                   min_ulen: int = 1,
+                   max_ulen: Optional[int] = None):
+    """Show a distribution of unit lengths in `reads`.
+
+    positional arguments:
+      @ reads : A list of TRRead objects.
+
+    optional arguments:
+      @ by             : Must be one of {"total", "count"}.
+      @ [min|max]_ulen : Range of unit length to count.
+    """
+    assert by in ("total", "count"), "`by` must be 'total' or 'count'"
+    ulens = [unit.length for read in reads for unit in read.units]
+    max_ulen = max(ulens) if max_ulen is None else max_ulen
+    ulen_counts = Counter(list(filter(lambda x: min_ulen <= x <= max_ulen,
+                                      ulens)))
+    if by == "total":
+        ulen_counts = {ulen: ulen * count
+                       for ulen, count in ulen_counts.items()}
+    show_plot([make_scatter(**dict(zip(('x', 'y'),
+                                       zip(*sorted(ulen_counts.items())))),
+                            mode="lines",
+                            name="Line graph<br>(for zooming out)",
+                            show_legend=True),
+               make_hist(ulen_counts,
+                         name="Bar graph<br>(for zooming in)",
+                         show_legend=True)],
+              make_layout(title=("Total bases for each unit length" if by == "total"
+                                 else "Number of units for each unit length"),
                           x_title="Unit length [bp]",
-                          y_title="Unit length * unit count [bp]",
-                          x_range=(min_ulen, max_ulen)),
-              out_fname=out_fname)
+                          y_title=("Total bases [bp]" if by == "total"
+                                   else "Frequency"),
+                          x_range=(min_ulen, max_ulen)))
