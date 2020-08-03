k-units clustering; small fix on setup.cfg

src/overlapper/split_merge_dpmm.py
@@ -22,6 +22,7 @@ class SplitMergeDpmmOverlapper:
       @ th_ward        : Distance threshold for initial clustering.
       @ alpha          : Concentration hyperparameter for DPMM.
       @ p_error        : Average sequencing error rate.
+      @ k_for_unit     : k-monomers are used for clustering.
       @ split_init_how : Specify how initial assignments of a split proposal
                          are decided.
                          Must be one of {"random", "nearest"}.
@@ -47,6 +48,7 @@ class SplitMergeDpmmOverlapper:
     th_ward: float = 0.01
     alpha: float = 1.
     p_error: float = 0.01
+    k_for_unit: int = 10
     split_init_how: str = "nearest"
     merge_how: str = "perfect"
     scheduler: Scheduler = Scheduler()
@@ -68,6 +70,7 @@ class SplitMergeDpmmOverlapper:
             shared_args=dict(th_ward=self.th_ward,
                              alpha=self.alpha,
                              p_error=self.p_error,
+                             k_for_unit=self.k_for_unit,
                              split_init_how=self.split_init_how,
                              merge_how=self.merge_how),
             scheduler=self.scheduler,
@@ -84,6 +87,7 @@ def run_smdc_multi(sync_reads: List[Tuple[int, List[TRRead]]],
                    th_ward: float,
                    alpha: float,
                    p_error: float,
+                   k_for_unit: int,
                    split_init_how: str,
                    merge_how: str,
                    n_core: int) -> List[Tuple[int, List[TRRead]]]:
@@ -94,6 +98,7 @@ def run_smdc_multi(sync_reads: List[Tuple[int, List[TRRead]]],
                                    th_ward,
                                    alpha,
                                    p_error,
+                                   k_for_unit,
                                    split_init_how,
                                    merge_how)
                                   for read_id, reads in sync_reads]))
@@ -104,20 +109,24 @@ def run_smdc(read_id: int,
              th_ward: float,
              alpha: float,
              p_error: float,
+             k_for_unit: int,
              split_init_how: str,
              merge_how: str,
              n_core: int = 1,
              plot: bool = False) -> Tuple[int, List[TRRead]]:
     assert all([read.synchronized for read in reads]), "Synchronize first"
-
-    units = [unit_seq for read in reads for unit_seq in read.unit_seqs]
-    quals = [qual for read in reads for qual in read.unit_quals]
+    # Collect all k-monomers in the reads
+    # TODO: XXX: properly treat insertions between units
+    units, quals = [], []
+    for read in reads:
+        units += [read.seq[read.units[i].start:read.units[i + k_for_unit - 1].end]
+                  for i in range(len(read.units) - k_for_unit + 1)]
+        quals += [read.qual[read.units[i].start:read.units[i + k_for_unit - 1].end]
+                  for i in range(len(read.units) - k_for_unit + 1)]
     smdc = ClusteringSeqSMD(units, quals, alpha, p_error)
-
     assert len(smdc.data) == len(smdc.quals)
     for i in range(smdc.N):
         assert len(smdc.data[i]) == len(smdc.quals[i])
-
     # Initial clustering
     smdc.calc_dist_mat(n_core=n_core)
     if plot:
@@ -125,11 +134,10 @@ def run_smdc(read_id: int,
     smdc.cluster_hierarchical(threshold=th_ward)
     logger.debug(f"Hierarchical clustering assignments:\n{smdc.assignments}")
     # TODO: remove single "outlier" units?
-    # (probably from regions covered only once by these reads right here)
+    # (probably from regions covered only once)
     smdc.gibbs_full()
     smdc.normalize_assignments()
     logger.debug(f"Assignments after full-scan Gibbs:\n{smdc.assignments}")
-
     # Perform split-merge samplings until convergence
     prev_p = f"{smdc.logp_clustering():.0f}"   # use str for -np.inf
     p_counts = Counter()   # for oscillation
@@ -163,15 +171,25 @@ def run_smdc(read_id: int,
     logger.debug(f"Finished read {read_id}")
 
     # Update representative units and assignments
+    # NOTE: k-monomers are assigned to each monomer as representative units
     er = EdlibRunner("global", revcomp=False)
     repr_units = {cluster_cons.cluster_id: cluster_cons.seq
                   for cluster_cons in smdc._generate_consensus()}
-    index = 0
     for read in reads:
         read.repr_units = repr_units
+        # Reset info on representative units since boundary (k-1) units should
+        # not have such info
         for unit in read.units:
+            unit.repr_id = None
+            unit.repr_aln = None
+    index = 0
+    for read in reads:
+        for i in range(len(read.units) - k_for_unit + 1):
+            k_unit_seq = read.seq[read.units[i].start:
+                                  read.units[i + k_for_unit - 1].end]
+            assert smdc.data[index] == k_unit_seq
+            unit = read.units[i]
             unit.repr_id = smdc.assignments[index]
-            unit.repr_aln = er.align(read.seq[unit.start:unit.end],
-                                     repr_units[unit.repr_id])
+            unit.repr_aln = er.align(k_unit_seq, repr_units[unit.repr_id])
             index += 1
     return read_id, reads

src/overlapper/split_merge_dpmm_core.py
@@ -66,18 +66,32 @@ class ClusteringSeqSMD(ClusteringSeq):
         # self._const_gibbs = -np.log10(self.N - 1 + self.alpha)
 
     def show(self):
+        # Compute a consensus sequence of all seqs to highlight variations
+        # among cluster consensus sequences
+        all_cons = consed.consensus(list(self.data),
+                                    seed_choice="median",
+                                    n_iter=3)
+        if all_cons == "":
+            logger.info("Use alt_consensus for all_cons")
+            all_cons = consensus_alt(list(self.data),
+                                     seed_choice="median")
         for cons in sorted(self._generate_consensus(),
                            key=lambda x: x.cluster_id):
-            print(f"Cluster {cons.cluster_id} "
+            print(f"### Cluster {cons.cluster_id} "
                   f"({cons.length} bp cons, {cons.cluster_size} seqs):")
+            aln = self.er.align(all_cons, cons.seq)
+            aln.show(a_name="master",
+                     b_name="cons",
+                     twist_plot=True)
+            print("===")
             seq_ids = self.cluster(cons.cluster_id, return_where=True)
             assert len(seq_ids) == cons.cluster_size
             for seq_id in seq_ids:
                 aln = self.er.align(cons.seq, self.data[seq_id])
-                print("---")
                 aln.show(a_name="cons",
                          b_name=f"seq{seq_id}",
                          twist_plot=True)
+                print("---")
 
     @property
     def cluster_ids(self) -> List[int]:
@@ -158,7 +172,7 @@ class ClusteringSeqSMD(ClusteringSeq):
              + self.n_clusters * np.log10(self.alpha)
              + np.sum([self._log_factorial[self.cluster_size(cluster_id) - 1]
                        for cluster_id in self.cluster_ids]))
-        #logger.debug(f"logp ewens = {p:.0f}")
+        # logger.debug(f"logp ewens = {p:.0f}")
         return p
 
     def _logp_cluster(self,
@@ -420,8 +434,12 @@ class ClusteringSeqSMD(ClusteringSeq):
             for x in self.cluster(old_cluster_id, return_where=True):
                 if x == data_i:
                     continue
-                dist_i = svs_dist(x, data_i)
-                dist_j = svs_dist(x, data_j)
+                # With cache
+                # dist_i = svs_dist(x, data_i)
+                # dist_j = svs_dist(x, data_j)
+                # Without cache
+                dist_i = self.er.align(self.data[x], self.data[data_i]).diff
+                dist_j = self.er.align(self.data[x], self.data[data_j]).diff
                 self.assignments[x] = (old_cluster_id if dist_i <= dist_j
                                        else new_cluster_id)
 